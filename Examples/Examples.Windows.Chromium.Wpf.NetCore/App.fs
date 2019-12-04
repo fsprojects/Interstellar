b99b6ca8f9440bf8bbe7643389503662295e9e95
John Wostenberg <jwosty@users.noreply.github.com>
2019-12-04 08:51:23 -0600

    Create an example project

---
+++ Examples/Examples.Windows.Chromium.Wpf.NetCore/App.fs
@@ -0,0 +1,26 @@
+﻿namespace Example.Windows.Chromium.Wpf
+open System
+open System.Windows
+open System.Windows.Controls
+
+type MainWindow() =
+    inherit Window()
+    
+    override this.OnInitialized (e: EventArgs) =
+        base.OnInitialized e
+        let closeButton = new Button(Content = "Close")
+        this.Content <- closeButton
+        closeButton.Click.Add (fun e -> this.Close ())
+
+type App() =
+    inherit Application()
+    override this.OnStartup(e: StartupEventArgs) =
+        base.OnStartup e
+        let window = new MainWindow()
+        window.Show ()
+
+module Main =
+    [<EntryPoint; STAThread>]
+    let main argv =
+        let app = new App()
+        app.Run ()
