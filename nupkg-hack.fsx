bede88a8135d161378e5acb77fab75759c197800
John Wostenberg <jwosty@users.noreply.github.com>
2020-01-07 18:39:43 -0700

    Write some code that can hack the nuspec project package version constraints

---
+++ nupkg-hack.fsx
@@ -0,0 +1,41 @@
+#r "System.IO.Compression"
+open System
+open System.IO
+open System.IO.Compression
+open System.Text
+open System.Text.RegularExpressions
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+let changeVersionConstraints text =
+    Regex("(?<=id=\"Interstellar.+?\"\\s+version=\")[^[\\]]*?(?=\")")
+        .Replace (text, MatchEvaluator(fun m -> sprintf "[%s]" m.Value))
+
+#if INTERACTIVE
+"""<group targetFramework=".NETFramework4.7.2">
+    <dependency id="Interstellar.Chromium" version="0.2.0-rc" exclude="Build,Analyzers" />
+    <dependency id="Interstellar.Core" version="0.2.0-rc" exclude="Build,Analyzers" />
+    <dependency id="CefSharp.Wpf" version="75.1.143" exclude="Build,Analyzers" />
+    <dependency id="FSharp.Core" version="4.2.3" exclude="Build,Analyzers" />
+</group>
+<group targetFramework=".NETCoreApp3.0">
+    <dependency id="Interstellar.Chromium" version="0.2.0-rc" exclude="Build,Analyzers" />
+    <dependency id="Interstellar.Core" version="0.2.0-rc" exclude="Build,Analyzers" />
+    <dependency id="CefSharp.Wpf" version="75.1.143" exclude="Build,Analyzers" />
+    <dependency id="FSharp.Core" version="4.2.3" exclude="Build,Analyzers" />
+</group>"""
+|> changeVersionConstraints |> printfn "%s"
+#endif
+
+let hackNupkgAtPath path =
+    use file = File.Open (path, FileMode.Open, FileAccess.ReadWrite, FileShare.None)
+    use archive = new ZipArchive(file, ZipArchiveMode.Update)
+    let nuspecName = sprintf "%s.nuspec" (Path.GetFileNameWithoutExtension path)
+    let oldEntry = archive.GetEntry nuspecName
+    let input =
+        (use nuspecReader = new StreamReader(oldEntry.Open(), Encoding.UTF8) in nuspecReader.ReadToEnd())
+    let output = changeVersionConstraints input
+    use nuspecWriter = new StreamWriter(oldEntry.Open(), Encoding.UTF8)
+    nuspecWriter.Write output
+
+hackNupkgAtPath (Path.Combine ("artifacts", "Interstellar.Wpf.Chromium.nupkg"))
