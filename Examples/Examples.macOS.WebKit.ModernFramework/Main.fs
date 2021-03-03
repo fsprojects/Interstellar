0e26e3fc4eaed33a6dae6aa882350081ae118c35
jwosty <jwosty@users.noreply.github.com>
2021-03-02 21:35:12 -0600

    macOS - demonstrate issue #17 (cant use insterstellar from xam.mac full framework app)

---
+++ Examples/Examples.macOS.WebKit.ModernFramework/Main.fs
@@ -0,0 +1,12 @@
+﻿namespace Example.macOS.WebKit
+
+open AppKit
+open System
+
+module main =
+    [<EntryPoint>]
+    let main args =
+        NSApplication.Init()
+        NSApplication.SharedApplication.Delegate <- new AppDelegate()
+        NSApplication.Main(args)
+        0
